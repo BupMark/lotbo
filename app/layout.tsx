78e9699337503e8f3fcc8465b697d089c246b4f6
Handgod <sambayo23@gmail.com>
2026-05-07 20:45:57 -0400

    Service Worker PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,14 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Lotbo" />
+
+        <script dangerouslySetInnerHTML={{__html: `
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js')
+    })
+  }
+`}} />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
