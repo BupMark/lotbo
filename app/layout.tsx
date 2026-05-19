b33a370e20bbd26e2f6cd706d8d4e90b76d75590
Handgod <sambayo23@gmail.com>
2026-05-19 11:57:22 -0400

    feat: Google Site Verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        <meta name="google-site-verification" content="LYkKnqzqIwJdg0CkKi6in88SravlwejUaTcftoucHLE" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#C8431A" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
