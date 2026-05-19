d0ca1b669b850c518abfaaa83e363c3c16e7e3a9
Handgod <sambayo23@gmail.com>
2026-05-19 12:05:11 -0400

    fix: Google Site Verification — nouvelle balise préfixe URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
-        <meta name="google-site-verification" content="LYkKnqzqIwJdg0CkKi6in88SravlwejUaTcftoucHLE" />
+ <meta name="google-site-verification" content="8wSKUg7KlocohGadZaoocuZIJiMhGtwWUtf8UNwzais" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#C8431A" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
